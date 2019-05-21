e9e14fbe52c39b88b65ea8dbf96b44713a5aab90
kumbasar <vkumbasar@outlook.com>
2019-05-21 14:29:15 +0300

    display value
    Signed-off-by: kumbasar <vkumbasar@outlook.com>

--- pie.py
+++ pie.py
@@ -4,16 +4,16 @@ import matplotlib.pyplot as plt
 
 # The slices will be ordered and plotted counter-clockwise.
 labels = [r'Pass', r'Fail', r'Skip']
-sizes = [200, 10, 1]
+sizes = [90, 9, 1]
 colors = ['yellowgreen', 'red' , 'blue']
-explode=(0, 0.05, 0,)
+explode=(0, 0.05, 0.05,)
 fig = plt.figure()
-patches, texts = plt.pie(sizes, explode=explode ,autopct=None, shadow=True, colors=colors, startangle=90)
+patches, texts = plt.pie(sizes, explode=explode, autopct=None, shadow=True, colors=colors, startangle=90)
 plt.suptitle('Test Scenarios')
 
-plt.legend(patches, labels, loc="center right")
+plt.legend( loc = 'center right', labels=['%s, %1.1f %%' % (l, s) for l, s in zip(labels, sizes)])
 fig.set_figheight(2)
-fig.set_figwidth(4)
+fig.set_figwidth(5)
 
 # Set aspect ratio to be equal so that pie is drawn as a circle.
 plt.axis('equal')
