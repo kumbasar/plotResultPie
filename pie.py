9603db6d03bf79ad40f5d8c2c0dddb458fd8d43c
kumbasar <vkumbasar@outlook.com>
2019-05-21 12:24:17 +0300

    3d pie
    Signed-off-by: kumbasar <vkumbasar@outlook.com>

--- pie.py
+++ pie.py
@@ -6,11 +6,12 @@ import matplotlib.pyplot as plt
 labels = [r'Pass', r'Fail', r'Skip']
 sizes = [200, 10, 1]
 colors = ['yellowgreen', 'red' , 'blue']
+explode=(0, 0.05, 0,)
 fig = plt.figure()
-patches, texts = plt.pie(sizes, colors=colors, startangle=90)
-plt.suptitle('Scenarios')
+patches, texts = plt.pie(sizes, explode=explode ,autopct=None, shadow=True, colors=colors, startangle=90)
+plt.suptitle('Test Scenarios')
 
-plt.legend(patches, labels, loc="right")
+plt.legend(patches, labels, loc="center right")
 fig.set_figheight(2)
 fig.set_figwidth(4)
 
